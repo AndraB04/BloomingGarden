bda32aeddd8f9af1acc39cd8fab8218b17eb78a1
AndraB <andra.bimbirica04@e-uvt.ro>
2025-05-06 16:30:07 +0300

    changed home page

--- angular_shop/src/app/home/home.component.ts
+++ angular_shop/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import {Router} from "@angular/router";
 import {ListProductsComponent} from "../list-products/list-products.component";
 import {CartButtonComponent} from "./cart-button/cart-button.component";
 import {CustomerService} from "../services/customer.service";
-import {NgIf} from "@angular/common";
+import {NgFor, NgIf} from "@angular/common";
 import {ConfigurationsService} from "../services/configurations.service";
 
 @Component({
@@ -20,18 +20,32 @@ import {ConfigurationsService} from "../services/configurations.service";
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    ListProductsComponent,
     CartButtonComponent,
-    NgIf
+    NgIf,
+    NgFor
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  banners = [
+    {
+      imageUrl: 'https://i.pinimg.com/736x/70/6c/92/706c9219f11f0eef6671126929e5e5b0.jpg',
+      redirectUrl: '/products',
+      buttonText: 'Shop Now'
+    },
+    {
+      imageUrl: 'https://i.pinimg.com/736x/c9/e2/1a/c9e21a41c9eb47a538b403e9d5ba392e.jpg',
+      redirectUrl: '/auth',
+      buttonText: 'Create an account'
+    }
+  ];
 
-  constructor(public appConfig: ConfigurationsService, private router:Router , public customerService:CustomerService){
-
-  }
+  constructor(
+    public appConfig: ConfigurationsService,
+    private router: Router,
+    public customerService: CustomerService
+  ) {}
   isUserAdmin(){
     if(this.customerService.getLoggedUser() != null && this.customerService.getLoggedUser().userRole == "ADMIN"){
       return true;
@@ -45,4 +59,8 @@ export class HomeComponent {
   onLogOut(){
     this.router.navigate(['/','auth']);
   }
+  navigateTo(url: string) {
+    this.router.navigate([url]);
+  }
+
 }
